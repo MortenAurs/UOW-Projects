ba43c001dc643c5e70df088ceff765db6a45acb9
Morten <morten.aursland@gmail.com>
2019-08-21 19:52:06 +1000

    Implemented own reverse function in linear search

--- Algorithms and Data Structures/Assignment 1/src/ass1.java
+++ Algorithms and Data Structures/Assignment 1/src/ass1.java
@@ -3,23 +3,124 @@ import java.util.Scanner;
 
 public class ass1 {
 
-    private static String[] dictList = new String[370103];
+    private static String[] dictList = new String[370104];
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
-
         readDictFile();
-        //linearSearch();
+        linearSearch();
         //binarySearch();
-        SpellCheck sc = new SpellCheck();
-        readSampleFile();
-        sc.spellCheck();
+        //readSampleFile();
         long endTime = System.currentTimeMillis();
-        System.out.println("Execution time is " + (endTime - startTime) + " milliseconds");
+        System.out.println("Execution time is " + (endTime - startTime)  + " milliseconds");
 
     }
 
-    public static void readSampleFile(){
+    // Reading through dictionary.txt and adding the contents to dictList-array
+    public static void readDictFile() {
+        int i = 0;
+        String dictFile = "dictionary.txt";
+        BufferedReader file;
+        try {
+            file = new BufferedReader(new FileReader(dictFile));
+            String line = file.readLine();
+            // Reading files as long as there is content
+            while (line != null) {
+                dictList[i] = line;
+                line = file.readLine();
+                i++;
+            }
+            file.close();
+            System.out.println("Number of words in the dictionary: " + i);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } catch (IOException e) {
+            System.out.println("Readline error");
+        }
+    }
+    // Finding the first five emordnilaps and printing them out
+    public static void linearSearch() {
+        String reversedWord;
+        int counter = 0;
+        int i = 0;
+        System.out.println("Five first emordnilap words (linear search):");
+        // Iterating every word in the dictionary list until it finds the five first emordnilaps'
+        while (counter < 5) {
+            reversedWord = reverse(dictList[i]);
+            // Iterating through the dictionary list again to find a match with the reversed word
+            for (int j = 0; j < dictList.length-1; j++){
+                // Checking that the word is more than one letter and if the reversed word exists in the list
+                if (dictList[j].length() > 1 && dictList[j].equals(reversedWord)) {
+                    // Print out the original word and the reversed word while incrementing
+                    // the emornilaps-found-counter by one
+                    System.out.println(dictList[i] + " : " + reversedWord);
+                    counter++;
+                }
+            }
+            i++;
+        }
+    }
+
+    // Reversing word and returning it
+    public static String reverse(String word) {
+        String reversedWord = "";
+        for (int i = 0, j = (word.length()-1); i < word.length(); i++, j--) {
+            reversedWord = reversedWord+word.charAt(j);
+        }
+        return reversedWord;
+    }
+
+    public static void binarySearch() {
+        StringBuilder reversedWord;
+        int counter = 0;
+        int length = 0;
+        int i = 0;
+        String longestWord = null;
+        System.out.println("First 10 emordnilap's (binary search):");
+        // Iterating every word in the dictionary list until it finds the ten first emordnilaps'
+        while (counter < 10) {
+            int result;
+            // Reverse the word we will be searching for
+            reversedWord = new StringBuilder(dictList[i]).reverse();
+            // Checking if word has more than 1 letter
+            if (dictList[i].length() > 1) {
+                result = search(reversedWord);
+                // Checking if result has a positive value
+                if (result > 0) {
+                    // Finding the longest word
+                    if (dictList[result].length() > length) {
+                        length = dictList[result].length();
+                        longestWord = dictList[result];
+                    }
+                    counter++;
+                    System.out.println(dictList[i] + " : " + dictList[result]);
+                }
+            }
+            i++;
+        }
+        System.out.println("Longest emordnilap is " + longestWord + " which is " + longestWord.length() + " characters long.");
+    }
+
+    public static int search(StringBuilder reversedWord) {
+        int low = 0;
+        int high = dictList.length - 1;
+        int mid;
+        // Going through list
+        while (low <= high) {
+            mid = (low + high) / 2;
+            if (dictList[mid].compareTo(String.valueOf(reversedWord)) < 0) {
+                low = mid + 1;
+            } else if (dictList[mid].compareTo(String.valueOf(reversedWord)) > 0) {
+                high = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+
+    public static void readSampleFile() {
         String sampleFile = "sample.txt";
         String[] sampleList = new String[700];
         String[] validWords = new String[700];
@@ -30,14 +131,14 @@ public class ass1 {
         int validCounter = 0;
         try {
             input = new Scanner(new File(sampleFile));
-            while(input.hasNext()){
+            while (input.hasNext()) {
                 found = false;
                 sampleList[i] = input.next().toLowerCase().replaceAll("[,.!\"':?;-]", "");
-                for(String str : validWords){
-                    if(str != null){
+                for (String str : validWords) {
+                    if (str != null) {
                         //System.out.println("Str: " + str);
                         //System.out.println("Samplelist[i]: + " + sampleList[i]);
-                        if(sampleList[i].equals(str)){
+                        if (sampleList[i].equals(str)) {
                             //System.out.println("Funnet");
                             found = true;
                         }
@@ -46,7 +147,7 @@ public class ass1 {
                 }
                 for (int k = 0; k < dictList.length; k++) {
                     if (sampleList[i].equals(dictList[k])) {
-                        if(!found) {
+                        if (!found) {
                             validWords[uniqueCounter] = sampleList[i];
                             uniqueCounter++;
                         }
@@ -61,113 +162,5 @@ public class ass1 {
             ex.printStackTrace();
         }
 
-    }
-
-    public static void readDictFile(){
-        int counter = 0;
-        String dictFile = "dictionary.txt";
-
-        BufferedReader file;
-        try {
-            file = new BufferedReader(new FileReader(dictFile));
-            String line = file.readLine();
-            // Reading files as long as there is content
-            while (line != null) {
-                dictList[counter] = line;
-                line = file.readLine();
-                counter ++;
-            }
-            file.close();
-            System.out.println("Number of words in the dictionary: " + counter);
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found");
-        } catch (IOException e) {
-            System.out.println("Readline error");
-        }
-    }
-
-
-    public static void linearSearch() {
-        StringBuilder reversedWord;
-        int counter = 0;
-        int i = 0;
-        System.out.println("Five first emordnilap words (linear search:");
-        // Iterating every word in the dictionary list until it finds the five first emordnilaps'
-        while(counter < 5){
-            int j = 0;
-            // Reverse the word in the list and save it in reversedWord
-            reversedWord = new StringBuilder(dictList[i]).reverse();
-            // Iterating through the dictionary list again to find a match with the reversed word
-            while(j < dictList.length-1){
-                // Checking that the word is more than one letter and if the reversed word exists in the list
-                if(dictList[j].length() > 1 && dictList[j].equals(String.valueOf(reversedWord))){
-                    // Print out the original word and the reversed word
-                    System.out.println(dictList[i] + " : " + reversedWord);
-                    counter++;
-                }
-                j++;
-            }
-            i++;
-        }
-    }
-
-    public static void binarySearch() {
-        StringBuilder reversedWord;
-        int counter = 0;
-        int length = 0;
-        int i = 0;
-        String longestWord = null;
-        System.out.println("First 10 emordnilap's (binary search):");
-        // Iterating every word in the dictionary list until it finds the ten first emordnilaps'
-        while(counter < 10){
-            int result;
-            // Reverse the word we will be searching for
-            reversedWord = new StringBuilder(dictList[i]).reverse();
-            // Checking if word has more than 1 letter
-            if(dictList[i].length() > 1) {
-                result = search(reversedWord);
-                // Checking if result has a positive value
-                if(result > 0){
-                    // Finding the longest word
-                    if(dictList[result].length() > length){
-                        length = dictList[result].length();
-                        longestWord = dictList[result];
-                    }
-                    counter++;
-                    System.out.println(dictList[i] + " : " + dictList[result]);
-                }
-            }
-            i++;
-        }
-        System.out.println("Longest emordnilap is " + longestWord + " which is " + longestWord.length() + " characters long.");
-    }
-
-    public static int search(StringBuilder reversedWord){
-        int low = 0;
-        int high = dictList.length-1;
-        int mid;
-        // Going through list
-        while (low <= high){
-            mid = (low+high) / 2;
-            if(dictList[mid].compareTo(String.valueOf(reversedWord)) < 0) {
-                low = mid + 1;
-            }else if (dictList[mid].compareTo(String.valueOf(reversedWord)) > 0) {
-                high = mid -1;
-            }else{
-                return mid;
-            }
-        }
-        return -1;
-    }
-}
-
-class SpellCheck{
-
-
-    public SpellCheck(){
-
-    }
-
-    public void spellCheck(){
     }
 }
