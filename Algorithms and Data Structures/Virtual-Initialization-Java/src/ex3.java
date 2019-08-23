45b3e50b383dcd3b1513c4e839c96451c88340e8
Morten <morten.aursland@gmail.com>
2019-08-23 18:54:50 +1000

    Continued assignment

--- Algorithms and Data Structures/Virtual-Initialization-Java/src/ex3.java
+++ Algorithms and Data Structures/Virtual-Initialization-Java/src/ex3.java
@@ -9,9 +9,10 @@ import java.util.Scanner;
 
 public class ex3 {
 
-    private static String[] data = new String[100];
-    private static String[] forward = new String[100];
-    private static String[] backward = new String[100];
+    private static int[] data = new int[100];
+    private static int[] forward = new int[100];
+    private static int[] backward = new int[100];
+    private static int cnt = 0;
 
     public static void main(String[] args) {
         readFile();
@@ -20,13 +21,37 @@ public class ex3 {
     // Reading through dictionary.txt and adding the contents to dictList-array.
     public static void readFile() {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter filename: ");
-        String fileName = input.nextLine();
+        //System.out.println("Enter filename: ");
+        //String fileName = input.nextLine();
+        String fileName = "ex3.txt";
         Scanner file;
+        int what, where, probe;
         try {
             file = new Scanner(new File(fileName));
-            while (file.hasNextLine()) {
-                System.out.println(file.nextLine());
+            while (file.hasNext()) {
+
+                what = Integer.parseInt(file.next());
+                where = Integer.parseInt(file.next());
+                if(what == -1 && where == -1) {
+                    break;
+                }
+                data[where] = what;
+
+                forward[cnt] = where;
+                backward[where] = cnt;
+                cnt++;
+            }
+            while(file.hasNext()) {
+                probe = Integer.parseInt(file.next());
+                if(probe == -1) {
+                    break;
+                }
+                if(backward[probe] > 0) {
+                    System.out.println("Position " + probe + " has been initialized to value " + data[probe]);
+                }else{
+                    System.out.println("Position " + probe + " has not been initialized");
+                }
+
             }
             file.close();
         } catch (FileNotFoundException e) {
