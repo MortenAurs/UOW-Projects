421da9d68c114680676782fc60ffdb3558ce223a
Morten <morten.aursland@gmail.com>
2019-09-22 20:26:05 +1000

    Implemented search algorithm

--- Algorithms and Data Structures/Karp-Rabin String Search - Java/src/ex6.java
+++ Algorithms and Data Structures/Karp-Rabin String Search - Java/src/ex6.java
@@ -3,22 +3,66 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class ex6 {
+    public static int d = 256;
+    public static int prime = 101;
 
+
+    // Main function
     public static void main(String[] args) {
-        String dictFile = "ass6.txt";
+        String dictFile = "ex6.txt";
         Scanner file;
 
         try {
             file = new Scanner(new File(dictFile));
             String target = file.nextLine();
             String search = file.nextLine();
-            // Reading files as long as there is content
-            System.out.println(target);
-            System.out.println(search);
             file.close();
+            int n = target.length();
+            int m = search.length();
+
+            int h = pow(d, m-1);
+
+            int hash_s = hash(search, m);
+            int hash_t = hash(target, m);
+
+            for(int i = 0; i < n-m; i++){
+
+                if(hash_s == hash_t){
+                    if(search.equals(target.substring(i, i+m))){
+                        System.out.println("String \"" + search + "\" found starting at location: " + i);
+                    }
+
+                }
+                hash_t = roll(hash_t, target.charAt(i), target.charAt(i+m), h);
+            }
         } catch (FileNotFoundException e) {
             System.out.println("File not found");
             System.exit(0);
         }
     }
+
+    // Create rolling hash of next substring
+    private static int roll(int ht, int c1, int c2, int h) {
+        ht = (d * (ht - c1*h) + c2 ) % prime;
+        if(ht<0){
+            ht += prime;
+        }
+        return ht;
+    }
+
+    public static int pow(int d, int e){
+        int p = 1;
+        for(int i = 0; i < e; i++){
+            p=(p*d) % prime;
+        }
+        return p;
+    }
+    public static int hash(String str, int m){
+        int h=0;
+        for(int i = 0; i < m; i++) {
+            h = ((d * h) + str.charAt(i)) % prime;
+        }
+        return h;
+    }
+
 }
