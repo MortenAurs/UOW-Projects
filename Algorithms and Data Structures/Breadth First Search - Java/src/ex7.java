b54bc736ee6f17daf82f25ce105f9316530c8f47
Morten <morten.aursland@gmail.com>
2019-10-12 20:45:01 +1100

    Changed the code

--- Algorithms and Data Structures/Breadth First Search - Java/src/ex7.java
+++ Algorithms and Data Structures/Breadth First Search - Java/src/ex7.java
@@ -3,15 +3,16 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class ex7 {
+    private static Stack stack = new Stack();
 
     public static void main(String args[]) {
         Scanner file;
-        Stack stack = new Stack();
+
         String fileName = "ex7.txt";
 
         try {
             file = new Scanner(new File(fileName));
-            int counter = Integer.parseInt(file.next());
+            int starter = Integer.parseInt(file.next());
             while (file.hasNext()){
                 int item = Integer.parseInt(file.next());
                 stack.push(item);
@@ -27,6 +28,29 @@ public class ex7 {
             System.exit(0);
         }
     }
+
+    public int bfs (starter, adj){
+        int[] vList = new int[20];
+
+
+        stack.push(starter);
+        int i = 0;
+        while (!stack.isEmpty()){
+            int current = stack.pop();
+            vList[i] = current;
+
+        }
+        current = q.dequeue()
+        add current to v
+        for each n in adj(current)
+        if n is not in v then
+        q.enqueue(n)
+        fi
+                rof
+        elihw
+        return v
+
+    }
 }
 
 
@@ -34,9 +58,9 @@ public class ex7 {
 class Stack {
     private int inIndex = 0;
     private int outIndex = 0;
-    private int maxLength = 201;
+    private int maxLength = 100;
     private int cntItem = 0;
-    private int[] stack = new int[maxLength];
+    public int[] stack = new int[maxLength];
 
     public void push(int item){
         if(cntItem==maxLength){
